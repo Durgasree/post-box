39d8ee26ab9302e0a324f36201f5486050f38722
Durgasree Boddu <durgasree.boddu@capgemini.com>
2019-03-08 16:04:26 +0530

    postbox with ajax functionality in tasks

---
+++ README.md
@@ -0,0 +1,1 @@
+Post Box is rails application with post comment like actions
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+Task.delete_all
+
+Task.create!(description: 'This is a task description',
+             deadline: '2014-07-01 01:00:00')
+
+Task.create!(description: 'This is another task description',
+             deadline: '2014-07-02 01:00:00')
+
+Task.create!(description: 'Yet another task description',
+             deadline: '2014-07-03 01:00:00')
+
+Post.create!(content: 'This is post one')
+
+Post.create!(content: 'This is new post ')
+
+Post.create!(content: 'This is new post two')
